ffafcca4757dacceb86af79e080456befe615e81
Jeremy Koepke <JeremyKoepke@JeremyKoepke.com>
2017-06-12 21:12:59 -0600

    Project 3
    Prints a cube using println() statements

---
+++ Chapter 1/Project 3/PrintCube.java
@@ -0,0 +1,36 @@
+/*
+	Author Information
+	------------------
+	Jeremy Koepke
+	June 12, 2017
+	www.JeremyKoepke.com
+
+	Project Information
+	-------------------
+	Java Project Series
+	Chapter 1 Project 3
+	https://github.com/JK-Designs/Java-Exercises
+*/
+
+import java.util.Scanner;
+
+public class PrintCube
+{
+	public static void main(String[] args)
+	{
+		System.out.println("     **************");
+		System.out.println("    *            **");
+		System.out.println("   *            * *");
+		System.out.println("  *            *  *");
+		System.out.println(" *            *   *");
+		System.out.println("**************    *");
+		System.out.println("*            *    *");
+		System.out.println("*            *    *");
+		System.out.println("*            *    *");
+		System.out.println("*            *   *");
+		System.out.println("*            *  *");
+		System.out.println("*            * *");
+		System.out.println("*            **");
+		System.out.println("**************");
+	}
+}
