fd74be02878faf6d1eb4ccc80f5896c8e2275e93
Jeremy Koepke <JeremyKoepke@JeremyKoepke.com>
2017-06-12 20:59:46 -0600

    Second Project
    Second project from chapter one, which is a modification of the first project to add three numbers instead of two.

---
+++ Chapter 1/Project 2/ThreeNumbers.java
@@ -0,0 +1,35 @@
+/*
+	Author Information
+	------------------
+	Jeremy Koepke
+	June 12, 2017
+	www.JeremyKoepke.com
+
+	Project Information
+	-------------------
+	Java Project Series
+	Chapter 1 Project 2
+	https://github.com/JK-Designs/Java-Exercises
+*/
+
+import java.util.Scanner;
+
+public class ThreeNumbers
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello out there.");
+		System.out.println("I will add three numbers for you.");
+		System.out.println("Enter three whole numbers on a line:");
+
+		int n1, n2, n3;
+
+		Scanner keyboard = new Scanner(System.in);
+		n1 = keyboard.nextInt();
+		n2 = keyboard.nextInt();
+		n3 = keyboard.nextInt();
+
+		System.out.println("The sum of those three numbers is");
+		System.out.println(n1 + n2 + n3);
+	}
+}
