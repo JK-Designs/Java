77052f99851d51e7962da1d18c48e756c3b003b0
Jeremy Koepke <JeremyKoepke@JeremyKoepke.com>
2017-06-14 19:01:30 -0600

    Third Project

--- Chapter 1/Project 3/PrintCube.java
+++ Chapter 1/Project 3/PrintCube.java
@@ -10,6 +10,13 @@
 	Java Project Series
 	Chapter 1 Project 3
 	https://github.com/JK-Designs/Java-Exercises
+
+	Problem Description
+	-------------------
+	Write a Java program that displays the
+	following picture. Hint: Write a sequence
+	of println statements that display lines
+	of asterisks and blanks.
 */
 
 public class PrintCube
