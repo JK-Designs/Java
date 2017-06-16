d43b919ccb8ba1186db8ca08f6110fec43d9504f
Jeremy Koepke <JeremyKoepke@JeremyKoepke.com>
2017-06-15 21:15:41 -0600

    Fifth Project
    The completed fifth project using flow of control statements to determine which of the entered numbers is the larger one.

--- Chapter 1/Project 5/NumbersBetween.java
+++ Chapter 1/Project 5/NumbersBetween.java
@@ -10,6 +10,16 @@
 	Java Project Series
 	Chapter 1 Project 5
 	https://github.com/JK-Designs/Java-Exercises
+
+	Project Description
+	-------------------
+	Write a complete program for the problem
+	described in Exercise 10. (Write statements
+	that can be used in a Java program to read
+	two integers and display the number of integers
+	that lie between them, including the integers
+	themselves. For example, four integers are
+	between 3 and 6: 3, 4, 5, and 6.)
 */
 
 import java.util.Scanner;
@@ -27,7 +37,18 @@ public class NumbersBetween
 		n1 = keyboard.nextInt();
 		n2 = keyboard.nextInt();
 
-		count = (n1 - n2) + 1;
+		if(n1 > n2)
+		{
+			count = (n1 - n2) + 1;
+		}
+		else if (n1 < n2)
+		{
+			count = (n2 - n1) + 1;
+		}
+		else
+		{
+			count = 0;
+		}
 
 		System.out.println("Between " + n1 + " and " + n2 + " are " + count + " digits.");
 	}
